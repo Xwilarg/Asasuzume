8f8e2febe01e56da4b9bfabb1a4eba78cb10e465
Xwilarg <xwilarg@protonmail.com>
2023-03-02 03:30:13 +0100

    Split data into separate JSON files

--- index.php
+++ index.php
@@ -8,14 +8,15 @@ use Twig\Environment;
 $loader = new FilesystemLoader("templates");
 $twig = new Environment($loader);
 
-$data = json_decode(file_get_contents("assets/data.json"), true);
-if ($_GET["name"] !== "") {
-    $elem = current(array_filter($data, function($x) {
-        return $x["name"] == $_GET["name"];
-    }));
-    if ($elem) {
+$data = [];
+$targetName = $_GET["name"];
+foreach (new DirectoryIterator('./assets/data') as $fileInfo) {
+    if ($fileInfo->isDot()) continue;
+
+    $content = json_decode(file_get_contents("./assets/data/" . $fileInfo), true);
+    if (substr($fileInfo, 0, -5) === $targetName) {
         echo $twig->render("character.html.twig", [
-            "data" => $elem,
+            "data" => $content,
             "audio" => [
                 "bond1", "bond2", "bond3", "bond4", "bond5",
                 "lobby1", "lobby2", "lobby3", "lobby4", "lobby5", "lobby6", "lobby7", "lobby8",
@@ -24,6 +25,7 @@ if ($_GET["name"] !== "") {
         ]);
         return;
     }
+    array_push($data, $content);
 }
 
 echo $twig->render("index.html.twig", [
