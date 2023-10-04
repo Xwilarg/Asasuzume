0d95faf98481fe425aef83419f42ca86bb6d119d
Xwilarg <xwilarg@protonmail.com>
2023-10-04 23:12:24 +0200

    Add Avalonia sample project

---
+++ Asasuzume/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Asasuzume.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ Asasuzume/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace Asasuzume.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
