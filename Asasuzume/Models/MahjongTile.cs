c5d81bfe5cfd8fc1fdb1c85722ee089945dcdc30
Xwilarg <xwilarg@protonmail.com>
2023-10-05 02:09:18 +0200

    Add callback on tile click

--- Asasuzume/Models/MahjongTile.cs
+++ Asasuzume/Models/MahjongTile.cs
@@ -1,12 +1,20 @@
-﻿namespace Asasuzume.Models
+﻿using ReactiveUI;
+using System.Windows.Input;
+
+namespace Asasuzume.Models
 {
     public class MahjongTile
     {
         public MahjongTile(string imagePath)
         {
             ImagePath = imagePath;
+            OnTileSelected = ReactiveCommand.Create(() =>
+            {
+
+            });
         }
 
         public string ImagePath { set; get; }
+        public ICommand OnTileSelected { set; get; }
     }
 }
