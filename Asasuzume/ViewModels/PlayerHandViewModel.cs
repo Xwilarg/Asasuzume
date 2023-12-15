0cfa76f74265957c01cbc8ed8875544ae108f682
Xwilarg <xwilarg@protonmail.com>
2023-12-16 00:24:56 +0100

    Fix some bugs

--- Asasuzume/ViewModels/PlayerHandViewModel.cs
+++ Asasuzume/ViewModels/PlayerHandViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using Asasuzume.Models;
+using System.Collections.ObjectModel;
 
 namespace Asasuzume.ViewModels;
 
 public class PlayerHandViewModel : ViewModelBase
 {
     public APlayer Player { set; get; }
+
+    public ObservableCollection<MahjongTile> Discarded => Player.Discarded;
 }
--- Asasuzume/Views/PlayerHandView.axaml.cs
+++ Asasuzume/Views/PlayerHandView.axaml.cs
@@ -3,26 +3,27 @@ using Asasuzume.ViewModels;
 using Avalonia;
 using Avalonia.Data;
 using Avalonia.ReactiveUI;
+using ReactiveUI;
 
 namespace Asasuzume.Views
 {
     public partial class PlayerHandView : ReactiveUserControl<PlayerHandViewModel>
     {
         public static readonly StyledProperty<APlayer> PlayerProperty =
-        AvaloniaProperty.Register<PlayerHandView, APlayer>(nameof(Player), defaultValue: null,
+        AvaloniaProperty.Register<PlayerHandView, APlayer>("Player", defaultValue: null,
             defaultBindingMode: BindingMode.TwoWay);
 
-        public APlayer Player
-        {
-            set => ViewModel!.Player = value;
-            get => ViewModel!.Player;
-        }
+        public APlayer Player { get; set; }
 
         public PlayerHandView()
         {
-            ViewModel = new();
-
             InitializeComponent();
+
+            this.WhenActivated(_ =>
+            {
+                var p = GetValue(PlayerProperty);
+                ;
+            });
         }
     }
 }
